71d50079d741cb2b143f6c25277be275b1d74013
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2025-01-19 23:02:29 +0530

    Removed the obj attribute

--- defaultconst.java
+++ defaultconst.java
@@ -4,6 +4,6 @@ public class defaultconst {
         System.out.println("Bike is created\n");
     }
     public static void main(String[]args){
-        defaultconst b=new defaultconst(); //warning occurs due to the variable b, is never used later!
+        new defaultconst(); //warning occurs due to the variable b, is never used later!
     }
 }
