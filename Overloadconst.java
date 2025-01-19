918e5921a46c3c499468af19aee699ef39692cb4
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2025-01-19 23:17:52 +0530

    Overload Constructor

---
+++ Overloadconst.java
@@ -0,0 +1,17 @@
+public class Overloadconst {
+    Overloadconst(){
+        System.out.println("Hi.....!\n");
+    }
+    Overloadconst(int a){
+        System.out.println("a="+a);
+    }
+    Overloadconst(char c){
+        System.out.println("c="+c);
+    }
+    public static void main(String[] args) {
+        Overloadconst obj1=new Overloadconst();
+        Overloadconst obj2=new Overloadconst(10);
+        Overloadconst obj3=new Overloadconst('8');
+
+    }
+}
