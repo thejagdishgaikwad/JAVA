830cf58c8a2f80edd5723e2461951af88950b8aa
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2025-01-24 23:47:30 +0530

    1B

---
+++ OneB/sMethods.java
@@ -0,0 +1,18 @@
+package JAVA.OneB;
+
+public class sMethods {
+    static void greatest(){
+        int x=10;
+        int y=20;
+            if(x>y){
+                System.out.println("X Is Greatest!\n");
+            }
+            else{
+                System.out.println("Y Is Greatest!\n");
+            }
+            return;
+    }
+    public static void main(String[] args) {
+        sMethods.greatest();
+    }
+}
