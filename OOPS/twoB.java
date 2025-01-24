818d4c013e8075e5f3f8cc766da38a9655e51235
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2025-01-25 00:00:21 +0530

    Double Inheritence

---
+++ OOPS/twoB.java
@@ -0,0 +1,19 @@
+package JAVA.OOPS;
+class animal{
+    void eat(){
+        System.out.println("Eating.....!\n");
+    }
+}
+class dog extends animal{
+    void bark(){
+        System.out.println("Barking.....!\n");
+    }
+}
+
+public class twoB {
+    public static void main(String[] args) {
+        dog d = new dog();
+        d.bark();
+        d.eat();
+    }    
+}
